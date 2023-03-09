d7a01fbfa4df2c9bf27513c7b2a4c34fb5b21394
Nicholas Cory <nacory@iastate.edu>
2023-03-09 14:04:07 -0600

    Initial commit

---
+++ makePass.py
@@ -0,0 +1,10 @@
+import random
+lowerLetters = "abcdefghijklmnopqrstuvwxyz"
+upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+numbers = "0123456789"
+symbols = "!@#$%^&*()_-+={}[]|\;:,.<>/?\"\'"
+allChar = lowerLetters + upperLetters + numbers + symbols
+pass_len = int(input("Enter length for password: "))
+password = "".join(random.sample(allChar, pass_len))
+
+print("\nYour password is \n \n" + password + "\n")
