e88234f28a4f343dc19f9d4880059605135dc21c
Nicholas Cory <nacory@iastate.edu>
2024-02-06 21:36:35 -0600

    Added options for password characters

--- makePass.py
+++ makePass.py
@@ -5,7 +5,19 @@ upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 numbers = "0123456789"
 symbols = "!@#$%^&*()_-+={}[]|\;:,.<>/?\"\'"
 
-allChar = lowerLetters + upperLetters + numbers + symbols
+print("Password Options: 1 - Letters, 2 - Letters and Numbers, 3 - Letters, Numbers and Symbols")
+choice = int(input("Enter Number for Choice: "))
+
+if choice == 1:
+    allChar = upperLetters + lowerLetters
+elif choice == 2:
+    allChar = upperLetters + lowerLetters + numbers
+elif choice == 3:
+    allChar = upperLetters + lowerLetters + numbers + symbols
+
+
+
+# allChar = lowerLetters + upperLetters + numbers + symbols
 pass_len = int(input("Enter length for password: "))
 password = "".join(random.sample(allChar, pass_len))
 
